89dc6011ae6a18fdd0b44e571cfb3c09914a3167
João Pedro Souza <joaop.souza@granitopagamentos.com.br>
2022-05-25 13:52:41 -0300

    calculator complete

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ def create_window():
     btnSize = (6,3)
 
     layout = [
-        [sg.Text('Result:' , pad=(5,5))],
+        [sg.Text('Result:' , pad=(5,5) , key="-text-")],
         [sg.Button('Enter',expand_x=True) , sg.Button('Clear', expand_x=True)],
         [sg.Button(1 , size=btnSize) , sg.Button(2 , size=btnSize) , sg.Button(3 , size=btnSize) , sg.Button('*' , size=btnSize)],
         [sg.Button(4 , size=btnSize) , sg.Button(5 , size=btnSize) , sg.Button(6 , size=btnSize), sg.Button('+' , size=btnSize)],
@@ -22,6 +22,7 @@ def create_window():
 window = create_window()
 
 current_num = []
+full_operation = []
 
 while True:
     event, values = window.read()
@@ -32,15 +33,21 @@ while True:
     if event in ['0','1','2','3','4','5','6','7','8','9','.']:
         current_num.append(event)
         num_string = ''.join(current_num)
-        print(num_string)
+        window['-text-'].update(num_string)
 
     if event in ['+','-','*','/']:
-        print(event)
+        full_operation.append(''.join(current_num))
+        current_num = []
+        full_operation.append(event)
+        window['-text-'].update('')
 
     if event == 'Enter':
-        print(event)
-
+        full_operation.append(''.join(current_num))
+        result = eval(''.join(full_operation))
+        window['-text-'].update(result)
     if event == 'Clear':
-        print(event)
+        full_operation = []
+        current_num = []
+        window['-text-'].update('Result:')
 
 window.close()
